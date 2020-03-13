ee1f30afe96d69d72ac8f75db7166f3068569b42
root <root@DESKTOP-K0I13CQ.localdomain>
2020-03-12 21:18:49 -0400

    initial changes needed to get this started

---
+++ envVars.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+export COMPOSE_CONVERT_WINDOWS_PATHS=1
+export langAnalyzer="ambar_en"
+export dataPath="/mnt/disk1/db"
+export crawlerName="creepyCrawler"
+export ambarHostIpAddress="192.168.1.6"
+export pathToCrawl="/mnt/disk1/Library"
